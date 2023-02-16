94c36c1f18c9ec8b0a738a131d4adf0da77c27d8
adrianielestudiante <a.aguilarr.2018@alumnos.urjc.es>
2023-02-16 19:48:16 +0100

    Mustache now works
    Mustache implemented
    Common header and footer
    Assets move to static folder (+ static folder created)
    This is one of the 5 most significant changes of this fase. It must be.

---
+++ templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/MustacheController.java
@@ -0,0 +1,49 @@
+package es.codeurjc.hellowordvscode;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MustacheController {
+	
+	@GetMapping("/index")
+	public String index(Model model) {
+		return "index";
+	}
+
+	@GetMapping("/admin")
+	public String admin(Model model) {
+		return "admin";
+	}
+
+	@GetMapping("/destino")
+	public String destino(Model model) {
+		return "destino";
+	}
+	
+	@GetMapping("/Login")
+	public String login(Model model) {
+		return "Login";
+	}
+
+	@GetMapping("/personalArea")
+	public String personalArea(Model model) {
+		return "personalArea";
+	}
+
+	@GetMapping("/Signup")
+	public String signup(Model model) {
+		return "Signup";
+	}
+
+	@GetMapping("/valoraciones")
+	public String valoraciones(Model model) {
+		return "valoraciones";
+	}
+
+	@GetMapping("/error")
+	public String error(Model model) {
+		return "error";
+	}
+}
