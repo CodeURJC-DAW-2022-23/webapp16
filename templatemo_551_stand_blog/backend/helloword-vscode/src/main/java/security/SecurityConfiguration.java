3bed76dc5390e08ee09bae669bee661975c5742d
PaulaCuenca <paulacuenka@gmail.com>
2023-03-01 23:31:33 +0100

    avances seguridad

---
+++ templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/security/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+ 
+ @Override
+ protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+ 
+    String encodedPassword = encoder.encode("pass");
+ 
+    auth.inMemoryAuthentication().withUser("user").password(encodedPassword).roles("USER");
+}
+
+@Override
+protected void configure(HttpSecurity http) throws Exception {
+ 
+ // Security configuration
+ // Public pages
+    http.authorizeRequests().antMatchers("/").permitAll();
+    http.authorizeRequests().antMatchers("/login").permitAll();
+    http.authorizeRequests().antMatchers("/loginerror").permitAll();
+    http.authorizeRequests().antMatchers("/logout").permitAll();
+    // Private pages (all other pages)
+    http.authorizeRequests().anyRequest().authenticated();
+ 
+ // - Login and logout 
+ // - Other security configurations
+ }
+}
+
