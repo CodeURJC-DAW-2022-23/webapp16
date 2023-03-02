23abb26e6db6b263d9c138447ec604ee18bc7cde
PaulaCuenca <paulacuenka@gmail.com>
2023-03-02 19:48:47 +0100

    avance seguridad

--- templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/security/SecurityConfiguration.java
+++ templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,31 +10,46 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+   
+   @Value("${security.user}")
+   private String user;
+   
+   @Value("${security.encodedPassword}")
+   private String encodedPassword;
+   
+   @Override
+   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      PasswordEncoderFactories.createDelegatingPasswordEncoder();
+      auth.inMemoryAuthentication().withUser(user).password("{bcrypt}"+encodedPassword).roles("USER");
+   }
+   
+   @Override
+   protected void configure(HttpSecurity http) throws Exception {
  
- @Override
- protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      // Security configuration
+      // Public pages
+       http.authorizeRequests().antMatchers("/").permitAll();
+       http.authorizeRequests().antMatchers("/login").permitAll();
+       http.authorizeRequests().antMatchers("/loginerror").permitAll();
+       http.authorizeRequests().antMatchers("/logout").permitAll(); //CREAR PAGINA DE LOGOUT
+       http.authorizeRequests().antMatchers("/admin").hasAnyRole("ADMIN");
+       http.authorizeRequests().antMatchers("/personalArea").hasAnyRole("USER");
+      // Private pages (all other pages)
+      http.authorizeRequests().anyRequest().authenticated();
 
-    PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
- 
-    String encodedPassword = encoder.encode("pass");
- 
-    auth.inMemoryAuthentication().withUser("user").password(encodedPassword).roles("USER");
-}
-
-@Override
-protected void configure(HttpSecurity http) throws Exception {
- 
- // Security configuration
- // Public pages
-    http.authorizeRequests().antMatchers("/").permitAll();
-    http.authorizeRequests().antMatchers("/login").permitAll();
-    http.authorizeRequests().antMatchers("/loginerror").permitAll();
-    http.authorizeRequests().antMatchers("/logout").permitAll();
-    // Private pages (all other pages)
-    http.authorizeRequests().anyRequest().authenticated();
- 
- // - Login and logout 
- // - Other security configurations
- }
+      //Login form
+      http.formLogin().loginPage("/login"); //hay que poner la misma URL que en formulario del login
+      http.formLogin().usernameParameter("username"); //mismo nombre que en el formulario
+      http.formLogin().passwordParameter("password");
+      http.formLogin().defaultSuccessUrl("/personalArea");
+      http.formLogin().failureUrl("/loginerror");
+      
+      //Logout
+      http.logout().logoutUrl("/logout");
+      http.logout().logoutSuccessUrl("/");
+      // Disable CSRF at the mome
+      http.csrf().disable(); 
+      // - Other security configurations
+   }
 }
 
