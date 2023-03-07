4321435bfafb464800b5830fbafc0b2f1849b4a2
aleromtor <alejandro.romero.torres@gmail.com>
2023-03-07 23:35:24 +0100

    creation error solved

--- templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/Controller/testDataInitializer.java
+++ templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/Controller/testDataInitializer.java
@@ -1,3 +1,5 @@
+package es.codeurjc.hellowordvscode.Controller;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
