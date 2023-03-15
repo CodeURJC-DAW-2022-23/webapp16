8a1233631f09f8c287eac22e29557def1a665963
aleromtor <alejandro.romero.torres@gmail.com>
2023-03-15 20:17:19 +0100

    show destination photos in main

--- templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/HellowordVscodeApplication.java
+++ templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/HellowordVscodeApplication.java
@@ -5,8 +5,6 @@ import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import es.codeurjc.hellowordvscode.Controller.testDataInitializer;
-
 @SpringBootApplication
 public class HellowordVscodeApplication {
 
