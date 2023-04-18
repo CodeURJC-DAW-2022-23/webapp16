533ebec2e6631bb1024df0af960edc4b0f4c59cb
ismael2106 <ismaelalbertaparicio@gmail.com>
2023-04-18 09:48:13 +0200

    ajax

--- templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/Controller/DestinationController.java
+++ templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/Controller/DestinationController.java
@@ -1,11 +1,13 @@
 package es.codeurjc.hellowordvscode.Controller;
 
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,7 +16,6 @@ import es.codeurjc.hellowordvscode.Entitys.Destination;
 import es.codeurjc.hellowordvscode.Repositories.DestinationRepository;
 
 @Controller
-@RequestMapping("/index")
 public class DestinationController {
 
 
@@ -23,10 +24,19 @@ public class DestinationController {
     private DestinationRepository destinations;
     
     
-    @GetMapping("/")
-    public Page<Destination> getDestinations(@RequestParam(required = false) Pageable page) {
+   
+
+    @GetMapping("/destinations/{page}")
+    public String getDestinations(Model model, @PathVariable Integer page) {
        
+<<<<<<< Updated upstream
             return destinations.findAll(PageRequest.of(0, 2));
+=======
+        List <Destination> destiantionsList = destinations.findAll(PageRequest.of(page,10)).getContent();
+
+        model.addAttribute("destinations", destiantionsList);
+        return "mostrarDestino";
+>>>>>>> Stashed changes
         
     }
 
