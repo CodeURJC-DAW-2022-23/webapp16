d3047b4c9abc480d78854408a892327b93908056
adrianielestudiante <a.aguilarr.2018@alumnos.urjc.es>
2023-04-24 12:00:19 +0200

    Sinc up with remote
    I need to sinc my previous changes with remote,
    and I don't know whats the difference with the previous commit.

--- templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/Controller/MustacheController.java
+++ templatemo_551_stand_blog/backend/helloword-vscode/src/main/java/es/codeurjc/hellowordvscode/Controller/MustacheController.java
@@ -229,6 +229,12 @@ public class MustacheController {
 		return "crearViaje";
 	}
 
+	@PostMapping("/crearViaje")
+	public String publicarViaje(Model model){
+
+		return "redirect:personalArea";
+	}
+
 	@GetMapping("/editarViaje")
 	public String showEditarViaje(Model model){
 		return "editarViaje";
@@ -273,7 +279,7 @@ public class MustacheController {
 		usuario.setImageFile(null);
 		userRepository.save(usuario);
 		model.addAttribute("username", usuario);
-		return "personalArea";
+		return "redirect:/personalArea";
 	}
 
 	/*@GetMapping("/editarDestinos/{name}")
